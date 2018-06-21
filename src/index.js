875ff4208c3c820d2895cf05db2c668f12246d20
Carol Chung <cch5ng@gmail.com>
2018-06-20 20:33:20 -0600

    style: update code highlight stylesheet

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Docs from './docs/Docs';
-import '../node_modules/highlight.js/styles/ocean.css';
+import '../node_modules/highlight.js/styles/atelier-lakeside-light.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Docs />, document.getElementById('root'));
