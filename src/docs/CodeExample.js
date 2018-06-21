e141e7cf9de40bf3afeb66874b01ec3f0bb1db3a
Carol Chung <cch5ng@gmail.com>
2018-06-20 20:29:12 -0600

    uncomment ref to highlight.js; add stylesheet

--- src/docs/CodeExample.js
+++ src/docs/CodeExample.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//import hljs from 'highlight.js/lib/highlight'
-//import javascript from 'highlight.js/lib/languages/javascript'
+import hljs from 'highlight.js/lib/highlight'
+import javascript from 'highlight.js/lib/languages/javascript'
 
 // This way is easy, but adds 170K gzipped to bundle since all langs are included.
 // import Highlight from 'react-highlight';
 
 class CodeExample extends React.Component {
   componentDidMount() {
-    //hljs.registerLanguage('javascript', javascript);
-    //hljs.highlightBlock(this.element);
+    hljs.registerLanguage('javascript', javascript);
+    hljs.highlightBlock(this.element);
   }
 
   render() {
